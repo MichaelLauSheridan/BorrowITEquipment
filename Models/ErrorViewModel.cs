830458ae81db0c50a237dbdfd5140bdbb3964cda
Michael Lau <lau0@sheridancollege.ca>
2025-09-27 10:10:00 -0400

    Project Initialized

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BorrowITEquip.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
