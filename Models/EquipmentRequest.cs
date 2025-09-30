85988865ecc1a11d61f18c617e5af02736982b22
Michael Lau <lau0@sheridancollege.ca>
2025-09-30 19:24:17 -0400

    Milestone 3. Request form

--- Models/EquipmentRequest.cs
+++ Models/EquipmentRequest.cs
@@ -5,20 +5,36 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 namespace BorrowITEquip.Models
 {
+    public enum Role
+    {
+        Student = 0,
+        Professor = 1
+    }
+    public enum EquipmentType
+    {
+        Laptop = 0,
+        Phone = 1,
+        Tablet = 2,
+        Another = 3
+    }
     public class EquipmentRequest
     {
         [Required(ErrorMessage = "Please enter your name")]
         public string? Name { get; set; }
         [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
         public string? Email { get; set; }
         [Required]
         [RegularExpression(@"^\d{3}-\d{3}-\d{4}$", ErrorMessage = "Phone must be in the format xxx-xxx-xxxx")]
         public string? Phone { get; set; }
-        [Required]
-        public string? EquipmentType { get; set; }
+        [Required(ErrorMessage = "Please select your role")]
+        public Role Role { get; set; }
+        [Required(ErrorMessage = "Please select your equipment type")]
+        public EquipmentType EquipmentType { get; set; }
         [Required]
         public string? RequestDetails { get; set; }
-        [Required]
-        public string? Duration { get; set; }
+        [Required(ErrorMessage = "Please enter the duration")]
+        [Range(1, int.MaxValue, ErrorMessage = "Duration must be greater than 0")]
+        public int Duration { get; set; }
     }
 }
