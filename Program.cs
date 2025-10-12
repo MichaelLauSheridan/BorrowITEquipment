54b561a3234cb207265dd34faac55669d7050dfb
Michael Lau <lau0@sheridancollege.ca>
2025-10-12 17:27:44 -0400

    M1:•M1. EF Packages & Project Bootstrap

--- Program.cs
+++ Program.cs
@@ -1,27 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using BorrowITEquip.Data;
+using BorrowITEquip.Models.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// DbContext
+builder.Services.AddDbContext<FastEquipmentContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("FastEquipmentDb")));
+
+// Repositories
+builder.Services.AddScoped<IEquipmentRepository, EFEquipmentRepository>();
+builder.Services.AddScoped<IRequestRepository, EFRequestRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// (optional) simple dev-time migration apply
+using (var scope = app.Services.CreateScope())
 {
-    app.UseExceptionHandler("/Home/Error");
+    var env = scope.ServiceProvider.GetRequiredService<IWebHostEnvironment>();
+    if (env.IsDevelopment())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<FastEquipmentContext>();
+        db.Database.Migrate();
+    }
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
